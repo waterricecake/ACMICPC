e6d9887df677471b31baabe932c5ad587cba0159
waterricecake <91263263+waterricecake@users.noreply.github.com>
2022-09-22 21:17:47 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,30 +1,14 @@
-import sys
-class Node():
-    def __init__(self,val:int,prev=None,nxt=None):
-        self.val = val
-        self.prev = prev
-        self.nxt = nxt
+N,K = map(int,input().split())
+arr = [i for i in range(1,N+1)]
 
-n, k = map(int,sys.stdin.readline().rstrip().split())
+answer = []
+num = 0
+for t in range(N):
+    num += K-1  
 
-startnode = Node(1)
-node = startnode
-for i in range(2,n+1):
-    node.nxt = Node(i)
-    node.nxt.prev = node
-    node = node.nxt
-
-node.nxt = startnode
-startnode.prev = node
-node = startnode
-arr = [0] * n
-for i in range(n):
-    for _ in range(k-1):
-        node = node.nxt
-    arr[i] = node.val
-    node.prev.nxt = node.nxt
-    node.nxt.prev = node.prev
-    node = node.nxt
+    if num >= len(arr):
+        num = num%len(arr)
+ 
+    answer.append(str(arr.pop(num)))
     
-
-print("<"+", ".join(map(str,arr))+">")
+print("<",", ".join(answer)[:],">", sep='')
